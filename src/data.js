5cf0efe712545614a6ac629901e2054b06c5b304
FluffyRudy <81473138+FluffyRudy@users.noreply.github.com>
2024-01-07 22:51:25 +0545

    Add '$' prefix in prices

--- src/data.js
+++ src/data.js
@@ -12,115 +12,115 @@ const restaurant = {
 
 const foods = {
     mainCourse: [
-        ["Thai Red Sweet Potato Curry", "18"],
-        ["Asian Garlic Tofu", "15"],
-        ["Hibachi Chicken", "20"],
-        ["Chinese Hunan Chicken", "19"],
-        ["Chinese Style Mango Chicken", "21"],
-        ["Stir Fry Vegetables In Hot Garlic Sauce", "16"],
-        ["Maharashtrian Misal Pav", "14"],
-        ["Japanese Teriyaki Tilapia", "22"]
+      [ 'Thai Red Sweet Potato Curry', '$18' ],
+      [ 'Asian Garlic Tofu', '$15' ],
+      [ 'Hibachi Chicken', '$20' ],
+      [ 'Chinese Hunan Chicken', '$19' ],
+      [ 'Chinese Style Mango Chicken', '$21' ],
+      [ 'Stir Fry Vegetables In Hot Garlic Sauce', '$16' ],
+      [ 'Maharashtrian Misal Pav', '$14' ],
+      [ 'Japanese Teriyaki Tilapia', '$22' ]
     ],
     appetizer: [
-        ["Pickled Jalapeños", "10"],
-        ["Classy Beef Tartare", "12"],
-        ["Foie Gras Torchon", "15"],
-        ["Octopus", "14"],
-        ["Oysters Rockefeller", "13"],
-        ["Chicken Lettuce Wraps", "11"],
-        ["Blooming Onion", "9"],
-        ["Baked Buffalo Wings", "10"]
+      [ 'Pickled Jalapeños', '$10' ],
+      [ 'Classy Beef Tartare', '$12' ],
+      [ 'Foie Gras Torchon', '$15' ],
+      [ 'Octopus', '$14' ],
+      [ 'Oysters Rockefeller', '$13' ],
+      [ 'Chicken Lettuce Wraps', '$11' ],
+      [ 'Blooming Onion', '$9' ],
+      [ 'Baked Buffalo Wings', '$10' ]
     ],
     drinks: [
-        ["Mojito", "8"],
-        ["Margarita", "9"],
-        ["Pina Colada", "10"],
-        ["Bloody Mary", "8"],
-        ["Long Island Iced Tea", "11"],
-        ["Cosmopolitan", "9"],
-        ["Whiskey Sour", "8"],
-        ["Mint Julep", "10"]
+      [ 'Mojito', '$8' ],
+      [ 'Margarita', '$9' ],
+      [ 'Pina Colada', '$10' ],
+      [ 'Bloody Mary', '$8' ],
+      [ 'Long Island Iced Tea', '$11' ],
+      [ 'Cosmopolitan', '$9' ],
+      [ 'Whiskey Sour', '$8' ],
+      [ 'Mint Julep', '$10' ]
     ],
     horsDoeuvre: [
-        ["Smoked Salmon Canapés", "12"],
-        ["Mini Quiche Lorraine", "10"],
-        ["Bruschetta with Tomato and Basil", "8"],
-        ["Deviled Eggs", "7"],
-        ["Stuffed Mushrooms", "9"],
-        ["Prosciutto-Wrapped Asparagus", "11"],
-        ["Cheese and Crackers", "6"],
-        ["Shrimp Cocktail", "14"]
+      [ 'Smoked Salmon Canapés', '$12' ],
+      [ 'Mini Quiche Lorraine', '$10' ],
+      [ 'Bruschetta with Tomato and Basil', '$8' ],
+      [ 'Deviled Eggs', '$7' ],
+      [ 'Stuffed Mushrooms', '$9' ],
+      [ 'Prosciutto-Wrapped Asparagus', '$11' ],
+      [ 'Cheese and Crackers', '$6' ],
+      [ 'Shrimp Cocktail', '$14' ]
     ],
     soup: [
-        ["French Onion Soup", "7"],
-        ["Cream of Mushroom Soup", "6"],
-        ["Chicken Noodle Soup", "5"],
-        ["Tomato Soup", "5"],
-        ["Minestrone Soup", "6"],
-        ["Clam Chowder", "8"],
-        ["Lobster Bisque", "10"],
-        ["Butternut Squash Soup", "7"]
+      [ 'French Onion Soup', '$7' ],
+      [ 'Cream of Mushroom Soup', '$6' ],
+      [ 'Chicken Noodle Soup', '$5' ],
+      [ 'Tomato Soup', '$5' ],
+      [ 'Minestrone Soup', '$6' ],
+      [ 'Clam Chowder', '$8' ],
+      [ 'Lobster Bisque', '$10' ],
+      [ 'Butternut Squash Soup', '$7' ]
     ],
     salad: [
-        ["Caesar Salad", "8"],
-        ["Greek Salad", "7"],
-        ["Cobb Salad", "9"],
-        ["Caprese Salad", "8"],
-        ["Spinach Salad", "7"],
-        ["Waldorf Salad", "8"],
-        ["Nicoise Salad", "9"],
-        ["Chef's Salad", "9"]
+      [ 'Caesar Salad', '$8' ],
+      [ 'Greek Salad', '$7' ],
+      [ 'Cobb Salad', '$9' ],
+      [ 'Caprese Salad', '$8' ],
+      [ 'Spinach Salad', '$7' ],
+      [ 'Waldorf Salad', '$8' ],
+      [ 'Nicoise Salad', '$9' ],
+      [ "Chef's Salad", '$9' ]
     ],
     fish: [
-        ["Grilled Salmon", "15"],
-        ["Fish and Chips", "12"],
-        ["Shrimp Scampi", "16"],
-        ["Lobster Roll", "20"],
-        ["Fish Tacos", "13"],
-        ["Clam Linguine", "14"],
-        ["Crab Cakes", "18"],
-        ["Tuna Steak", "17"]
+      [ 'Grilled Salmon', '$15' ],
+      [ 'Fish and Chips', '$12' ],
+      [ 'Shrimp Scampi', '$16' ],
+      [ 'Lobster Roll', '$20' ],
+      [ 'Fish Tacos', '$13' ],
+      [ 'Clam Linguine', '$14' ],
+      [ 'Crab Cakes', '$18' ],
+      [ 'Tuna Steak', '$17' ]
     ],
     palateCleanser: [
-        ["Lemon Sorbet", "5"],
-        ["Ginger Granita", "5"],
-        ["Champagne Sorbet", "6"],
-        ["Green Apple Sorbet", "5"],
-        ["Mint Sorbet", "5"],
-        ["Cucumber Sorbet", "5"],
-        ["Pineapple Sorbet", "5"],
-        ["Coconut Sorbet", "5"]
+      [ 'Lemon Sorbet', '$5' ],
+      [ 'Ginger Granita', '$5' ],
+      [ 'Champagne Sorbet', '$6' ],
+      [ 'Green Apple Sorbet', '$5' ],
+      [ 'Mint Sorbet', '$5' ],
+      [ 'Cucumber Sorbet', '$5' ],
+      [ 'Pineapple Sorbet', '$5' ],
+      [ 'Coconut Sorbet', '$5' ]
     ],
     cheeseCourse: [
-        ["Brie", "7"],
-        ["Cheddar", "6"],
-        ["Gouda", "7"],
-        ["Blue Cheese", "8"],
-        ["Camembert", "7"],
-        ["Feta", "6"],
-        ["Mozzarella", "6"],
-        ["Swiss Cheese", "7"]
+      [ 'Brie', '$7' ],
+      [ 'Cheddar', '$6' ],
+      [ 'Gouda', '$7' ],
+      [ 'Blue Cheese', '$8' ],
+      [ 'Camembert', '$7' ],
+      [ 'Feta', '$6' ],
+      [ 'Mozzarella', '$6' ],
+      [ 'Swiss Cheese', '$7' ]
     ],
     dessert: [
-        ["Chocolate Mousse", "7"],
-        ["Apple Pie", "6"],
-        ["Cheesecake", "8"],
-        ["Tiramisu", "8"],
-        ["Creme Brulee", "7"],
-        ["Panna Cotta", "7"],
-        ["Brownie Sundae", "8"],
-        ["Lemon Tart", "7"]
+      [ 'Chocolate Mousse', '$7' ],
+      [ 'Apple Pie', '$6' ],
+      [ 'Cheesecake', '$8' ],
+      [ 'Tiramisu', '$8' ],
+      [ 'Creme Brulee', '$7' ],
+      [ 'Panna Cotta', '$7' ],
+      [ 'Brownie Sundae', '$8' ],
+      [ 'Lemon Tart', '$7' ]
     ],
     mignardise: [
-        ["Chocolate Truffles", "5"],
-        ["Macarons", "6"],
-        ["Petit Fours", "5"],
-        ["Madeleines", "5"],
-        ["Mini Eclairs", "6"],
-        ["Almond Biscotti", "5"],
-        ["Mini Cannoli", "6"],
-        ["Chocolate-Dipped Strawberries", "6"]
+      [ 'Chocolate Truffles', '$5' ],
+      [ 'Macarons', '$6' ],
+      [ 'Petit Fours', '$5' ],
+      [ 'Madeleines', '$5' ],
+      [ 'Mini Eclairs', '$6' ],
+      [ 'Almond Biscotti', '$5' ],
+      [ 'Mini Cannoli', '$6' ],
+      [ 'Chocolate-Dipped Strawberries', '$6' ]
     ]
-};
+  };
 
 export {restaurant, foods};
